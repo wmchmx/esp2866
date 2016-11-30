ae8728a39e7455c77cddd457d1ebc2be4b181417
wmchmx <wmchmx@gmail.com>
2016-11-30 17:31:34 +0100

    Add the libraries and tools

--- esp2866.sh
+++ esp2866.sh
@@ -2,10 +2,15 @@
 sudo apt-get --yes install update
 sudo apt-get --yes install upgrade
 sudo apt-get --yes install git autoconf build-essential gperf bison flex texinfo libtool libncurses5-dev wget gawk libc6-dev-amd64 python-serial libexpat-dev
-mkdir /opt/Espressif
-chown "$USER" opt/Espressif/
 
-cd /opt/Espressif
+ESP2866_BIN_ROOT="/opt/Espressif"
+ESP2866_SDK_NAME="ESP8266_SDK"
+ESP2866_SDK_ROOT="$ESP2866_BIN_ROOT""/""$ESP2866_SDK_NAME"
+
+mkdir "$ESP2866_BIN_ROOT"
+chown "$USER" "$ESP2866_BIN_ROOT"/
+
+cd "$ESP2866_BIN_ROOT"
 git clone -b lx106 git://github.com/jcmvbkbc/crosstool-NG.git 
 cd crosstool-NG
 ./bootstrap && ./configure --prefix="$PWD" && make && make install
@@ -14,7 +19,7 @@ cd crosstool-NG
 PATH_ADDITION="$PWD""/builds/xtensa-lx106-elf/bin"
 PATH="$PATH_ADDITION"":""$PATH"
 
-cd "/opt/Espressif"
+cd "$ESP2866_BIN_ROOT"
 SDK_NAME_PREFIX=esp_iot_sdk
 SDK_VERSION=1.2.0
 SDK_REVISION=15_07_03
@@ -22,8 +27,32 @@ SDK_ZIP_FILE="$SDK_NAME_PREFIX""_v""$SDK_VERSION""_""$SDK_REVISION"".zip"
 
 wget -O "$SDK_ZIP_FILE" "https://github.com/esp8266/esp8266-wiki/raw/master/sdk/""$SDK_ZIP_FILE"
 unzip "$SDK_ZIP_FILE"
-mv "$SDK_NAME_PREFIX""_v""$SDK_VERSION" ESP8266_SDK
-mv License ESP8266_SDK/
+mv "$SDK_NAME_PREFIX""_v""$SDK_VERSION" "$ESP2866_SDK_NAME"
+mv License "$ESP2866_SDK_NAME"/
+
+
+cd "$ESP2866_SDK_ROOT"
+sed -i -e 's/xt-ar/xtensa-lx106-elf-ar/' -e 's/xt-xcc/xtensa-lx106-elf-gcc/' -e 's/xt-objcopy/xtensa-lx106-elf-objcopy/' Makefile
+mv examples/IoT_Demo .
+
+cd "$ESP2866_SDK_ROOT"
+wget -O lib/libc.a https://github.com/esp8266/esp8266-wiki/raw/master/libs/libc.a
+wget -O lib/libhal.a https://github.com/esp8266/esp8266-wiki/raw/master/libs/libhal.a
+wget -O include.tgz https://github.com/esp8266/esp8266-wiki/raw/master/include.tgz
+tar -xvzf include.tgz
+rm -f include.tgz
+
+
+cd "$ESP2866_BIN_ROOT"
+wget -O esptool_0.0.2-1_i386.deb https://github.com/esp8266/esp8266-wiki/raw/master/deb/esptool_0.0.2-1_i386.deb
+dpkg -i esptool_0.0.2-1_i386.deb
+rm -f esptool_0.0.2-1_i386.deb
+
+cd "$ESP2866_BIN_ROOT"
+git clone "https://github.com/themadinventor/esptool esptool-py"
+ln -s "$PWD""/esptool-py/esptool.py" "crosstool-NG/builds/xtensa-lx106-elf/bin/"
+
+
 
 ENV_FILE="/home/""$USER""/esp2866-env.sh"
 echo "#!/bin/bash" > "$ENV_FILE"
